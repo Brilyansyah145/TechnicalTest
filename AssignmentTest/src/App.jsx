d678e07be9ec8f447c3c4e41b327cb975d42b5a3
Ade Octa <ade.octa.brilyansyah2@gmail.com>
2023-06-13 20:41:18 +0700

    perubahan

--- AssignmentTest/src/App.jsx
+++ AssignmentTest/src/App.jsx
@@ -3,37 +3,80 @@ import './App.css'
 
 
 const App = () => {
-  const [nilai, setNilai] = useState('');
+  const [inputData, setInputData] = useState([
+    { label: 'Mahasiswa 1', value: '' },
+    { label: 'Mahasiswa 2', value: '' },
+    { label: 'Mahasiswa 3', value: '' },
+    { label: 'Mahasiswa 4', value: '' },
+    { label: 'Mahasiswa 5', value: '' },
+    { label: 'Mahasiswa 6', value: '' },
+    { label: 'Mahasiswa 7', value: '' },
+    { label: 'Mahasiswa 8', value: '' },
+    { label: 'Mahasiswa 9', value: '' },
+    { label: 'Mahasiswa 10', value: '' },
+  ]);
 
-
-  const handleChangeNilai = (event) => {
-    setNilai(event.target.value);
+  const handleChangeNilai = (index, event) => {
+    const updatedData = [...inputData];
+    updatedData[index].value = event.target.value;
+    setInputData(updatedData);
   };
 
- 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log ('Data Penilaian:', inputData);
+  };
 
   return (
     <div>
+      <h1>Aplikasi Penilaian Mahasiswa</h1>
+      <form onSubmit={handleSubmit}>
       <table>
-        <td>
-          <tr>Mahasiswa 1</tr>
-        </td>
-        <td>
-          <tr><input type="number" placeholder='1' value={nilai} onChange={handleChangeNilai}/></tr>
-        </td>
-        <td><tr><input type="number" placeholder='1' value={nilai} onChange={handleChangeNilai}/></tr></td>
-        <td>
-          <tr><input type="number" placeholder='1' value={nilai} onChange={handleChangeNilai}/></tr>
-        </td>
-        <td>
-          <tr><input type="number" placeholder='1' value={nilai} onChange={handleChangeNilai}/></tr>
-        </td>
+        {inputData.map((mahasiswa, index) => (
+          <tr key={index}>
+            <td>{mahasiswa.label}</td>
+            <td>
+              <input
+                type="number"
+                placeholder="1"
+                value={mahasiswa.value}
+                onChange={(event) => handleChangeNilai(index, event)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                placeholder="1"
+                value={mahasiswa.value}
+                onChange={(event) => handleChangeNilai(index, event)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                placeholder="1"
+                value={mahasiswa.value}
+                onChange={(event) => handleChangeNilai(index, event)}
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                placeholder="1"
+                value={mahasiswa.value}
+                onChange={(event) => handleChangeNilai(index, event)}
+              />
+            </td>
+          </tr>
+        ))}
       </table>
+      <div><input type='submit' /></div>
+      </form>
     </div>
-    
-    
-  
   );
 };
 
 export default App;
+
+
